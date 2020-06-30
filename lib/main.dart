230b353857c99d5521e09e67a4443d88d83d3af0
Maciej Brażewicz <mb@kmd.dk>
2020-06-30 11:29:58 +0200

    stage 0

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WhatWeather app',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: ChooseLocationPage(title: 'Choose your city'),
+    );
+  }
+}
+
+class ChooseLocationPage extends StatefulWidget {
+  ChooseLocationPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ChooseLocationPageState createState() => _ChooseLocationPageState();
+}
+
+class _ChooseLocationPageState extends State<ChooseLocationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container()
+    );
+  }
+}
