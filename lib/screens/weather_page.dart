a1d0dcfdbc0f4b51adaea75281baf4d2f9062e83
Maciej Brażewicz <mb@kmd.dk>
2020-07-01 08:33:50 +0200

    weather icon moved

--- lib/screens/weather_page.dart
+++ lib/screens/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterworkshops/services/weather_service.dart';
+import 'package:flutterworkshops/widgets/wather_icon.dart';
 
 class WeatherPage extends StatefulWidget {
   static const String routeName = "/weather";
@@ -20,38 +21,7 @@ class _WeatherPageState extends State<WeatherPage> {
           title: Text("Weather in ${widget.cityName}"),
         ),
         //TODO extend with more weather info
-        body: Center(child: WeatherIcon(WeatherService.get().getWeather(widget.cityName).type))
+        body: Center(child: WeatherIcon(WeatherService.get().getWeather(widget.cityName)))
     );
-  }
-}
-
-class WeatherIcon extends StatelessWidget {
-  final WeatherType type;
-
-  WeatherIcon(this.type);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(_getUnicode(), style: Theme.of(context).textTheme.headline1,);
-  }
-
-  String _getUnicode() {
-    switch(type) {
-      case WeatherType.HeavyCloud:
-      case WeatherType.LightCloud:
-        return "⛅";
-      case WeatherType.Snow:
-      case WeatherType.Sleet:
-      case WeatherType.Hail:
-        return "🌨";
-      case WeatherType.Thunderstorm:
-      case WeatherType.Showers:
-        return "🌩";
-      case WeatherType.HeavyRain:
-      case WeatherType.LightRain:
-        return "🌧";
-      case WeatherType.Clear:
-        return "🌤";
-    }
   }
 }
